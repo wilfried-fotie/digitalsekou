5f1a7f8d3119d0198498d82bf67a759f57efc548
wilfried-fotie <fotie58@gmail.com>
2021-10-09 10:47:48 +0100

    backend update

--- global.jsx
+++ global.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 axios.defaults.headers.common["X-Auth-Token"] = "brrijLkcofcJVlwt-Qmd2xM-TXU"
-axios.defaults.baseURL = 'http://127.0.0.1:5000'
+axios.defaults.baseURL = 'http://18.189.171.36'
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*"
--- pages/Upload.jsx
+++ pages/Upload.jsx
@@ -1,74 +1,88 @@
 import React from 'react'
 import axios from 'axios'
 import "../global"
-import { File, Selector, SelectorValid } from '../components/FormTools';
+import { File } from '../components/FormTools';
 import { useForm } from 'react-hook-form';
 
-// function Upload() {
-//    
 
-//     const [state,setState] = React.useState({
-//         data: "",
-//         uri: ""
-
-//     });
-//     const handleImageChange = (e) => {
-//         let reader = new FileReader();
-//         reader.readAsDataURL(e.target.files[0])
-//         console.log((e.target.files[0].name).split(".", -1)[1])
-//         reader.onload = (ev) => {
-//             setState(s => ({
-//                 ...s,
-//                 uri: e.target.files[0],
-//                 data: ev.target.result}))
-//         };
-    
-//         // setState(s => {
-//         //     return {
-//         //         ...s,
-//         //         data: URL.createObjectURL(e.target.files[0])
-//         //     }
-//         // });
-
-//     } 
-    
-// const handleUploadImage = (ev) => {
-//     ev.preventDefault();
-   
-//     const formData = new FormData();
-
-//     formData.append("file", state.uri);
-
-
-
-//     axios
-//         .post("/upload", formData)
-//         .then(res => console.log(res))
-//         .catch(err => console.warn(err));
-// }
-//     return (
-//         <div>
-//             <form onSubmit={handleUploadImage}>
-//                 <File name="logo" onChange={handleImageChange}>Importer Le Logo</File>
-          
-//                 <br />
-
-//                 <SelectorValid opt={ options }/>
-//                 <div>
-//                     <button type="submit" className="btnPri">Upload</button>
-//                 </div>
-//                 <img src={state.data} alt="img" />
-//             </form>
-//         </div>
-//     )
-// }
-
-// export default Upload
-
+import S3 from 'react-aws-s3';
 
 
 
 function Upload() {
+
+   const config = {
+    bucketName: 'digitalsekou',
+    region: 'us-east-2',
+    accessKeyId: 'AKIAYMXWVHOCFUJ7HGPJ',
+    secretAccessKey: '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',
+    
+}
+
+const ReactS3Client = new S3(config);
+
+
+    const [state,setState] = React.useState({
+        data: "",
+        uri: ""
+
+    });
+    const handleImageChange = (e) => {
+        let reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0])
+        console.log((e.target.files[0].name).split(".", -1)[1])
+        reader.onload = (ev) => {
+            setState(s => ({
+                ...s,
+                uri: e.target.files[0],
+                data: ev.target.result}))
+        };
+    
+        // setState(s => {
+        //     return {
+        //         ...s,
+        //         data: URL.createObjectURL(e.target.files[0])
+        //     }
+        // });
+
+    } 
+    
+const handleUploadImage = (ev) => {
+    ev.preventDefault();
+   
+    const formData = new FormData();
+
+    formData.append("file", state.uri);
+
+
+ReactS3Client
+    .uploadFile(state.uri, "01-logo-profil")
+    .then(data => console.log("Upload fine"))
+    .catch(err => console.log("error" + err))
+}
+    return (
+        <div>
+            <form onSubmit={handleUploadImage}>
+                <File name="logo" onChange={handleImageChange}>Importer Le Logo</File>
+          
+                <br />
+
+             
+               
+                <button type="submit" className="btnPri">Sub</button> 
+                <img src={state.data ||  "https://digitalsekou.s3.us-east-2.amazonaws.com/01-logo-profil.jpeg"} alt="img" />
+               
+            </form>
+        </div>
+    )
+}
+
+export default Upload
+
+
+
+
+/*function Upload() {
     const options = [{ value: "zizi", label: "zozo" }, { value: "zizi", label: "zozo" }, { value: "zizi", label: "zozo" },]
     const { register, control, handleSubmit, formState: { errors, isSubmitSuccessful, isSubmitted, isSubmitting, isValid } } = useForm({ mode: "onTouched" })
     const [data, setData] = React.useState()
@@ -86,6 +100,5 @@ function Upload() {
             </form>
         </div>
     )
-}
+}*/
 
-export default Upload
